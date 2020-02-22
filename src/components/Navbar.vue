ded3d7c6d9fd67d91e9987b486e626669005ee5b
jessecodesNblends <ocran.jss@gmail.com>
2020-02-22 07:51:43 +0000

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-toolbar align="center" color="#FBC02D">
+      <v-toolbar-title>
+        <span class="font-weight-bold">CheckList</span>
+      </v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      dialog: false,
+      task: '',
+    };
+  },
+  methods: {
+    newTask() {
+        this.$emit('newTaskAdded', this.task);
+        this.task = ''
+    }
+  }
+};
+</script>
